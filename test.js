16484a75b589749e0bc5e200d7214b7578c9aa6a
Wasel Al-Mahri <33339924+waseeld@users.noreply.github.com>
2021-11-22 21:17:54 +0300

    Update test.js

--- test.js
+++ test.js
@@ -2,10 +2,14 @@ const Dotan = require('./index');
 const crypto = require('crypto');
 const hash = crypto.createHash("sha256");
 
-const pubKey = hash.update("This is Silly pas$word1", "utf-8").digest('hex');
+function sha256(msg){
+    return hash.update(msg, "utf-8").digest('hex');
+}
+
+const pubKey = sha256("This is Silly pas$word1");
 var dotan = new Dotan(pubKey);
 
-console.log("Public key we use:", pubKey);
+console.log("password hash we use:", pubKey);
 
 const msg = "Any Message LOL";
 var encryptedMsg = dotan.encrypted(msg);
@@ -17,11 +21,10 @@ console.log("decrypted:", decryptedMsg);
 var key = dotan.getKeyHex();
 console.log("key:", key);
 
-var get = dotan.getKeyBufferFromHex(key);
-console.log("Buffer:", get);
+var toBuffer = dotan.getKeyBufferFromHex(key);
+console.log("Buffer:", toBuffer);
 
-
-// Public key we use: eb29f0bfa86db05bc3d1b7367ae6bad91945a3579498004e399f1ebd6b4a7b9d
+// password hash we use: eb29f0bfa86db05bc3d1b7367ae6bad91945a3579498004e399f1ebd6b4a7b9d
 // encrypted: dd86260269c476d89bc4b9a98c6e7d0b
 // decrypted: Any Message LOL
 // key: 6016f0cb0c4c649ccc18192cd22ef3217eef43eaa052bf87
